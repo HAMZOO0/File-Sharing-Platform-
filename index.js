bf1ed41c51f9cc15e49db3119879cc4d4acd8856
Hamza Sajid <hamzakhan174722@gmail.com>
2024-07-04 10:59:07 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 3000; // Use environment variable or default to 3000
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // Set the views directory
+
 app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
 
 // Ensure 'upload' directory exists or handle it dynamically for production
