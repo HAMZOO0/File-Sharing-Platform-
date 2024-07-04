1a4b49d5c63dea915c6029b5387e6217dae05a50
Hamza Sajid <hamzakhan174722@gmail.com>
2024-07-03 22:57:19 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ const express = require('express');
 const path = require('path');
 const multer = require('multer');
 require('dotenv').config(); // Load environment variables
-const app = express();
 
-const PORT = process.env.PORT ; // Use environment variable or default to 3000
+const app = express();
+const PORT = process.env.PORT || 3000; // Use environment variable or default to 3000
 
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
 
-//When you add express.static, you're telling Express to serve static files (like images, CSS files, or uploaded files) from a specified directory. Here’s what this line of code does:
-
-app.use(express.static(path.join(__dirname, 'public'))); // for css
-
-app.use('/upload', express.static(path.join(__dirname, 'upload')));
+// Ensure 'upload' directory exists or handle it dynamically for production
+const uploadDir = path.join(__dirname, 'upload');
+app.use('/upload', express.static(uploadDir));
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -22,21 +21,19 @@ const storage = multer.diskStorage({
         cb(null, `${Date.now()}-${file.originalname}`);
     }
 });
+
 const upload = multer({ storage: storage });
 
-// Route to display the upload form
 app.get('/', (req, res) => {
     res.render('upload');
 });
 
-// Route to handle file upload and generate shareable link
 app.post('/upload', upload.single('fileuploader'), (req, res) => {
-  const fileName = req.file.filename;
-  const downloadUrl = `${req.protocol}://${req.get('host')}/download/${fileName}`;
+    const fileName = req.file.filename;
+    const downloadUrl = `${req.protocol}://${req.get('host')}/download/${fileName}`;
     res.render('link', { downloadUrl });
 });
 
-// Route to display the download page
 app.get('/download/:fileName', (req, res) => {
     const fileName = req.params.fileName;
     const fileUrl = `/upload/${fileName}`;
